69bcf43e43027b0c2e54ca38870775494fba2b37
felipe <felipesscreator@gmail.com>
2022-09-22 16:56:19 -0300

    feat: subscribe button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,30 @@
 import Head from 'next/head';
 
+import { SubscribeButton } from '../components/SubscribeButton';
+
+import styles from './home.module.scss';
+
 export default function Home() {
   return (
     <>
       <Head>
         <title>Home | NewsLetter</title>
       </Head>
-      <h1>
-        salve
-      </h1>
+      
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.99 month</span>
+          </p>
+
+          <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
 }
