ce2149eaa43c9c17f32268e178d0f46595c5b1b3
felipe <felipesscreator@gmail.com>
2022-09-22 12:23:52 -0300

    feat: global style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,12 @@
 import Head from 'next/head';
 
-import styles from '../styles/home.module.scss';
-
 export default function Home() {
   return (
     <>
       <Head>
         <title>Home | NewsLetter</title>
       </Head>
-      <h1 className={styles.title}>
+      <h1>
         salve
       </h1>
     </>
