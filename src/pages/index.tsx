8b2c59d4a101f76f6a1b32ce499ad66889ece3bf
felipe <felipesscreator@gmail.com>
2022-09-22 11:41:01 -0300

    feat: module css, sass

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,9 @@
+import styles from '../styles/home.module.scss';
+
 export default function Home() {
   return (
-    <h1>salve</h1>
+    <h1 className={styles.title}>
+      salve
+    </h1>
   )
 }
