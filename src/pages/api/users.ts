51e57ee14df2978729d3790a8e5e4c6170e98e8b
felipe <felipesscreator@gmail.com>
2022-09-23 15:21:21 -0300

    feat: serverless

---
+++ src/pages/api/users.ts
@@ -0,0 +1,12 @@
+import { NextApiRequest, NextApiResponse } from "next"
+
+export default (request: NextApiRequest, response: NextApiResponse) => {
+  const users = [
+    {
+      id: 1,
+      name: "John",
+    }
+  ]
+
+  return response.json(users)
+}
