67126b0f9d9061ef1edc3d244eedf8aff7baf18e
felipe <felipesscreator@gmail.com>
2022-09-29 11:35:53 -0300

    feat: callback faunadb comented failing

--- src/services/fauna.ts
+++ src/services/fauna.ts
@@ -1,5 +1,12 @@
+import NextAuth from "next-auth";
 import { Client } from 'faunadb';
+import { FaunaAdapter } from "@next-auth/fauna-adapter";
 
 export const fauna = new Client({
   secret: process.env.FAUNADB_KEY
+})
+
+export default NextAuth({
+  providers: [],
+  adapter: FaunaAdapter(fauna)
 })
