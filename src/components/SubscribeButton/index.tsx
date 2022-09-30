05071d5ceafdc186342409df8515e99bf653978e
felipe <felipesscreator@gmail.com>
2022-09-30 13:25:05 -0300

    feat: stripe chechout session, redirect

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,8 @@
+import { signIn, useSession } from 'next-auth/react';
+
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
+
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -5,10 +10,32 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const { data: session } = useSession();
+
+  async function handleSubscribeButton() {
+    if (!session) {
+      signIn('github')
+      return
+    }
+
+    try {
+      const response = await api.post('/subscribe')
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (err) {
+      alert(err.message);
+    }
+  }
+
   return (
     <button 
       type="button"
       className={styles.subscribeButton}
+      onClick={handleSubscribeButton}
     >
       Subscribe now
     </button>
