242f77607838df959fa3e2c4cfea4a5001fb8da8
felipe <felipesscreator@gmail.com>
2022-10-17 16:01:09 -0300

    feat: add posts styles

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,5 +1,38 @@
+import Head from 'next/head';
+
+import styles from './styles.module.scss';
+
 export default function Posts() {
   return(
-    <h1>hello</h1>
+    <>
+      <Head>
+        <title>Posts | NewsLetter</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          <a href='#'>
+            <time>12 de março de 2021</time>
+            <strong>Lorem ipsum dolor tempore nihil.</strong>
+            <p>Lorem ipsum dolor sit amet consectetur adi autem nobis obcaecati placeat non, id sint natus, accusantium corrupti.</p>
+          </a>
+          <a href='#'>
+            <time>12 de março de 2021</time>
+            <strong>Lorem ipsum dolor tempore nihil.</strong>
+            <p>Lorem ipsum dolor sit amet consectetur adi autem nobis obcaecati placeat non, id sint natus, accusantium corrupti.</p>
+          </a>
+          <a href='#'>
+            <time>12 de março de 2021</time>
+            <strong>Lorem ipsum dolor tempore nihil.</strong>
+            <p>Lorem ipsum dolor sit amet consectetur adi autem nobis obcaecati placeat non, id sint natus, accusantium corrupti.</p>
+          </a>
+          <a href='#'>
+            <time>12 de março de 2021</time>
+            <strong>Lorem ipsum dolor tempore nihil.</strong>
+            <p>Lorem ipsum dolor sit amet consectetur adi autem nobis obcaecati placeat non, id sint natus, accusantium corrupti.</p>
+          </a>
+        </div>
+      </main>
+    </>
   );
 }
