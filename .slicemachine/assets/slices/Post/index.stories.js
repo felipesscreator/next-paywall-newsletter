6f636263c2ffae2dfcfbfab6c85a1da7fb9f3b0d
felipe <felipesscreator@gmail.com>
2022-10-17 15:38:05 -0300

    feat: add slicemachine for prismicCMS and create route posts

---
+++ .slicemachine/assets/slices/Post/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/Post';
+
+export default {
+  title: 'slices/Post'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{"title":"go","descricao":"said"}],"primary":{},"slice_type":"post","id":"_Default"}} />
+_Default.storyName = ''
---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,5 @@
+export default function Posts() {
+  return(
+    <h1>hello</h1>
+  );
+}
