ec9ba429074e7c4d3718f0ccc696219aed8578e6
felipe <felipesscreator@gmail.com>
2022-10-19 20:15:27 -0300

    feat: add post by id styles

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,5 +1,65 @@
-export default function Post() {
+import { GetServerSideProps } from "next"
+import { getSession } from "next-auth/react"
+import { Head } from "next/document";
+
+import { createClient } from "../../services/prismic"
+
+import styles from './post.module.scss'
+
+interface PostProps {
+  post: {
+    slug: string;
+    title: string;
+    content: string;
+    updatedAt: string;
+  }
+}
+
+export default function Post({ post }: PostProps) {
   return(
-    <h1></h1>
+    <>
+      <Head>
+        <title>{post.title} | Newsletter</title>
+      </Head>
+
+      <main className={styles.container}>
+        <article className={styles.post}>
+          <h1>{post.title}</h1>
+          <time>{post.updatedAt}</time>
+          <div
+            className={styles.postContent}
+            dangerouslySetInnerHTML={{ __html: post.content }} 
+          />
+        </article>
+      </main>
+    </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ req, params }) => {
+  const session = await getSession({ req })
+  const { slug } = params;
+
+  // if (!session) {}
+
+  const prismic = createClient(req);
+
+  const response = await prismic.getAllByUIDs('post', String(slug), {})
+
+  const post = {
+    slug,
+    title: response,
+    content: response,
+    updatedAt: new Date(response.last_publication_date).toLocaleDateString('pt-BR', {
+      day: '2-digit',
+      month: 'long',
+      year: 'numeric'
+    })
+  }
+
+  return {
+    props: {
+      post,
+    }
+  }
 }
